1f7f07e4dc1f8a1277ee28c0dd100827e87963f0
MOHAMED IDMANSOUR <mi.idmansour@gmail.com>
2021-06-06 22:01:34 +0100

    add basic of POO

--- bin/dart.dart
+++ bin/dart.dart
@@ -14,7 +14,7 @@ class Employee {
       empSalary = 80;
   }
 
-  showEmpInfo() {
+  void showEmpInfo() {
     print('Employee Name Is : $empName');
     print('Employee Age Is : $empAge');
     print('Employee Salary Is : $empSalary');
