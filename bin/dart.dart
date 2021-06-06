8066bc027728524a29902de6cb39b2dc916fa515
MOHAMED IDMANSOUR <mi.idmansour@gmail.com>
2021-06-06 19:58:34 +0100

    func in dart

--- bin/dart.dart
+++ bin/dart.dart
@@ -1,5 +1,5 @@
 void main(List<String> arguments) {
-  var firstName = 'Mohamed', lastName = 'Idm';
+  /*var firstName = 'Mohamed', lastName = 'Idm';
   var age = 23;
   print('${firstName.length} ' + lastName.toUpperCase());
   print('$age');
@@ -7,5 +7,22 @@ void main(List<String> arguments) {
   int? nb2 = 20;
   print(int.tryParse('-10')!.isNegative);
   print(nb2.isNegative);
-  print(nb2.isNaN);
+  print(nb2.isNaN);*/
+
+  // function arrow
+  var x = '100U';
+  print(getSum(100, 100));
+  print(getSum(double.tryParse(x) != null ? double.parse(x) : 0, 100).abs());
+
+  print (getLastName());
+
+  print(double.infinity);
 }
+
+double getSum(double x, double y) => x + y;
+
+String getName(){
+  return 'fff';
+}
+
+String getLastName() => 'getLastName';
