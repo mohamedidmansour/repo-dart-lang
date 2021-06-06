c5f1c2a91d42f5c10fe49e8b6149706fa98cc2da
MOHAMED IDMANSOUR <mi.idmansour@gmail.com>
2021-06-06 14:31:18 +0100

    feat: added operations test String operations and more

--- bin/dart.dart
+++ bin/dart.dart
@@ -1,3 +1,11 @@
 void main(List<String> arguments) {
-  print('Hello world!');
+  var firstName = 'Mohamed', lastName = 'Idm';
+  var age = 23;
+  print('${firstName.length} ' + lastName.toUpperCase());
+  print('$age');
+
+  int? nb2 = 20;
+  print(int.tryParse('-10')!.isNegative);
+  print(nb2.isNegative);
+  print(nb2.isNaN);
 }
